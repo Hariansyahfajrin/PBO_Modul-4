519f2b7313fb06fe3da5cb8342e634d18686f5c6
Hariansyahfajrin <114633787+Hariansyahfajrin@users.noreply.github.com>
2022-11-17 06:54:05 +0700

    Add files via upload

---
+++ latihan1.java
@@ -0,0 +1,17 @@
+/* menentukan variabel 
+ */
+package Hariansyahfajrin1992022;
+
+/**
+ *
+ * @author ASUS
+ */
+public class latihan1 {
+    public static void main(String[] args) {
+        int value = 10;
+        char x;
+        x = 'A';
+    System.out.println(value);
+    System.out.println( "The value of x=" +  x );
+ }
+} 
---
+++ tugas4.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Hariansyahfajrin1992022;
+
+/**
+ *
+ * @author ASUS
+ */
+public class tugas4 {
+      public static void main(String[] args){
+           
+            System.out.println ("(a/((b^c)^d))–(e+f)–(g*h)+i");
+            System.out.println("(3*10*2)/15-2+((4^2)^2)");
+            System.out.println("(r^s)*t/(u-v)+(w^x)-y++");
+      }
+}
